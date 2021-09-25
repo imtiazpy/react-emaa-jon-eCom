02f386045740380826b39b883732037f6e311364
imtiazpy <ahmed.imti1994@gmail.com>
2021-09-25 15:15:06 +0600

    Addedd star rating

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import Rating from 'react-rating';
 import './Product.css';
 const Product = (props) => {
     const element = <FontAwesomeIcon icon={faShoppingCart} />
-    const { name, img, price, seller, stock } = props.product;
+    const { name, img, price, seller, stock, star } = props.product;
     return (
         <div className="product">
             <div>
@@ -14,6 +15,12 @@ const Product = (props) => {
                 <p>by: {seller}</p>
                 <h4>${price}</h4>
                 <p>only {stock} left in stock - order soon</p>
+                <Rating
+                    initialRating={star}
+                    readonly
+                    emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color" />
+                <br /> <br />
                 <button
                     className="btn-cart"
                     onClick={() => props.handleAddToCart(props.product)}
